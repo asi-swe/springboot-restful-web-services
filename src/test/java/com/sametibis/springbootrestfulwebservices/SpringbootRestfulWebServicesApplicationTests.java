59d7d23445563b773f05a5b7435f3a573de7d5b3
Samet Ibis <sametibis@Samet-MacBook-Pro.local>
2023-01-28 22:26:42 -0600

    initial commit

---
+++ src/test/java/com/sametibis/springbootrestfulwebservices/SpringbootRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametibis.springbootrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
